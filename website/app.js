1415b10704ad83e3072f54ad23ccdb5aa5ddc75c
Amr Fayez <amrfayez.247@gmail.com>
2023-06-26 17:18:14 +0300

    Re-polishing

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 // Personal API Key for OpenWeatherMap API
-const apiKey = "95361ca0d959cbf6fc3ab73a1fbb6921&units=metric";
+const apiKey = "95361ca0d959cbf6fc3ab73a1fbb6921";
 
 // Date
 let d = new Date();
@@ -8,32 +8,44 @@ let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 // Eventlistener to generate the result upon click on button
 let generate = document.getElementById('generate');
 generate.addEventListener('click',()=>{
-  let zip = document.querySelector('#zip').value;
-  webData(zip)
+  let city = document.querySelector('#city').value;
+  webData(city)
 });
 
 // functions to get web API data
-async function webData (zip){
-  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}`);
+async function webData (city){
+  city = city.trim();
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
   const data = await res.json()
   .then(function(data) {
-      postData('/add', {temp:data.main.temp, date:newDate, content: feelings.value});
+      postData('/add', {
+        desc: data.weather[0].description,
+        temp:data.main.temp,
+        date:newDate,
+        content: feelings.value, 
+        city: city
+      });
       display(data);
     });
 };
 
 // function to display the data in our page
 const display= async()=>{
-  const response = await fetch("/all")
-    try{
+const response = await fetch("/all")
+  try{
     const allData = await response.json();
+    document.getElementById("name").textContent="City: "+allData.city;
     document.getElementById("date").textContent="Date: "+allData.date;
     document.getElementById("temp").textContent="Temp: "+allData.temp+" \u00B0C";
+    document.getElementById("feel").textContent="Description: "+allData.desc;
     document.getElementById("response").textContent="Feeling: "+allData.content;
-    } catch(error) {
-      console.log("error", error);
-    }
-  };
+
+    document.querySelector('#city').value = "";
+    document.querySelector('#feelings').value = "";
+  } catch(error) {
+    console.log("error", error);
+  }
+};
 
 
 /* Function to POST data */
